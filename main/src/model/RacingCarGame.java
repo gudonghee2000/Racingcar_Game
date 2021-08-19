698e572cd3bac62b1fcacb0dbba26dd42f6438f9
gudonghee2000 <gudonghee2000@gmail.com>
2021-08-19 14:53:09 +0900

    feat: 입력한 자동차의 예외상황을 확인하는 메소드 생성

--- main/src/model/RacingCarGame.java
+++ main/src/model/RacingCarGame.java
@@ -1,8 +1,6 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -13,11 +11,23 @@ public class RacingCarGame {
         inputRacingCars(Cars);
     }
 
-    private void inputRacingCars(String[] Cars) {
-        racingCars = Arrays.stream(Cars).map(Car::new).collect(Collectors.toList());
+    private void inputRacingCars(String[] cars) {
+        isValidationCars(cars);
+        racingCars = Arrays.stream(cars).map(Car::new).collect(Collectors.toList());
     }
 
-    private void isValidationCars(){
-        
+    private static void isValidationCars(String[] cars) {
+        if ((int) Arrays.stream(cars).filter(car -> car.contains(" ")).count() > 0) {
+            throw new IllegalArgumentException("[Error] 입력한 자동차 중 이름에 공백이 있는 자동차가 있습니다.");
+        }
+        if ((int) Arrays.stream(cars).filter(car -> car.length() > 5).count() > 0) {
+            throw new IllegalArgumentException("[Error] 입력한 자동차 중 이름이 5자를 넘는 자동차가 있습니다.");
+        }
+        if (Arrays.stream(cars).distinct().count() != cars.length) {
+            throw new IllegalArgumentException("[Error] 입력한 자동차 중 이름이 중복되는 자동차가 있습니다.");
+        }
+        if (cars.length == 1) {
+            throw new IllegalArgumentException("[Error] 입력한 자동차가 한대입니다.");
+        }
     }
 }
