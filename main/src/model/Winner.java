d6cd9d2d31894303c382b0ff5174bb1e7c415778
gudonghee2000 <gudonghee2000@gmail.com>
2021-08-20 14:47:08 +0900

    feat : 우승자 정보를 가질 객체 생성

---
+++ main/src/model/Winner.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Winner {
+    private List<Car> Winners = new ArrayList<>();
+
+    protected Winner(List<Car> r) {
+    }
+}
